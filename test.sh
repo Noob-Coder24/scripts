d548127b93b4868f59ff053fe634564592b5a909
Noob-Coder24 <kevinaaaquil@gmail.com>
2022-03-10 22:39:03 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-mkdir files
-touch hello.txt
-mv hello.txt files
+mkdir hacked
+cd hacked
+curl -o whoami.txt https://raw.githubusercontent.com/Noob-Coder24/scripts/main/whoami.txt
+cd ..
+rm -rf test.sh
